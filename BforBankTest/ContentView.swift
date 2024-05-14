5d361ebe2300aa3c6b2474361c9439b389e1945a
User <khalil.mhelheli@gmail.com>
2024-05-15 01:21:01 +0200

    Create refactory method for making scan button

--- BforBankTest/ContentView.swift
+++ BforBankTest/ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
     
+    
+    
     var body: some View {
         NavigationView {
             ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
@@ -19,16 +21,7 @@ struct ContentView: View {
                         Button(action: {
                             
                         }) {
-                            Text("Scannez")
-                                .padding()
-                                .background(Color.getBackgroundColor(colorScheme: colorScheme))
-                                .foregroundColor(.green)
-                                .frame(width: 200, height: 40)
-                                .cornerRadius(10)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.green, lineWidth: 1) // Border line
-                                )
+                            makeScanButtonView()
                         }
                         .buttonStyle(PlainButtonStyle())
                        Spacer()
@@ -42,6 +35,19 @@ struct ContentView: View {
             
             
         }
+    }
+    
+    private func makeScanButtonView() -> some View {
+        return Text("Scannez")
+            .padding()
+            .background(Color.getBackgroundColor(colorScheme: colorScheme))
+            .foregroundColor(.green)
+            .frame(width: 200, height: 40)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.green, lineWidth: 1)
+            )
     }
 }
 
