2d1be4c4a83e67cb313c1a9483e7182928876e93
User <khalil.mhelheli@gmail.com>
2024-05-14 20:29:53 +0200

    Create prject

---
+++ BforBankTest/BforBankTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BforBankTestApp.swift
+//  BforBankTest
+//
+//  Created by User on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BforBankTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
