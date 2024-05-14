b1e84e6a7e946bfe0833c8e8b8e376193e788aa9
User <khalil.mhelheli@gmail.com>
2024-05-15 01:14:43 +0200

    Create the first screen UI

--- BforBankTest/ContentView.swift
+++ BforBankTest/ContentView.swift
@@ -1,24 +1,58 @@
-//
-//  ContentView.swift
-//  BforBankTest
-//
-//  Created by User on 14/05/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
+                Color.getBackgroundColor(colorScheme: colorScheme)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Text("Scannez, importez ou saisissez l'IBAN")
+                        .padding(.leading, 20)
+                        .navigationBarTitle("Ajouter un bénéficiaire", displayMode: .inline)
+                    
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Text("Scannez")
+                                .padding()
+                                .background(Color.getBackgroundColor(colorScheme: colorScheme))
+                                .foregroundColor(.green)
+                                .frame(width: 200, height: 40)
+                                .cornerRadius(10)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.green, lineWidth: 1) // Border line
+                                )
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                       Spacer()
+                    }
+                    .padding()
+        
+                }
+                
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            
+            
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+extension Color {
+    static func getBackgroundColor(colorScheme: ColorScheme) -> Color {
+        return colorScheme == .dark ? Color.black : Color.white
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
